513288af1a505d8d48ee252f9639a987c9868a0b
miguelpombodev <mp96dev@gmail.com>
2022-01-15 19:50:03 -0300

    First Commit - jest-styled-components added

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles';
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e StyledComponents
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor de frente para um tela de código
+    "
+    />
+  </S.Wrapper>
+);
+
+export default Main;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps /*, AppContext */ } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="" />
+        <meta name="description" content="Simples NextJS app" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
